79685747e95380e27d8ac52dc733a7fe08c9d4e2
Maxim Bilan <maximb.mail@gmail.com>
2015-01-17 13:25:50 +0200

    Added obj c compatibility in swift project

---
+++ swift_project/ios_swift_mix/ObjCViewController.h
@@ -0,0 +1,13 @@
+//
+//  ObjCViewController.h
+//  ios_swift_mix
+//
+//  Created by Maxim Bilan on 1/17/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ObjCViewController : UIViewController
+
+@end
---
+++ swift_project/ios_swift_mix/ObjCViewController.m
@@ -0,0 +1,38 @@
+//
+//  ObjCViewController.m
+//  ios_swift_mix
+//
+//  Created by Maxim Bilan on 1/17/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import "ObjCViewController.h"
+
+@interface ObjCViewController ()
+
+@end
+
+@implementation ObjCViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"Obj C Controller");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
