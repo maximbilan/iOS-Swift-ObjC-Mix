fe1233e51561d0086c5240e5b5d8b4456e3dbd62
Maxim Bilan <maximb.mail@gmail.com>
2015-01-17 13:10:53 +0200

    Implemented swift compatibility in obj c project

---
+++ objc_project/ios_objc_mix/SwiftController.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftController.swift
+//  ios_objc_mix
+//
+//  Created by Maxim Bilan on 1/17/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+import UIKit
+
+@objc class SwiftController: UIViewController {
+
+}
