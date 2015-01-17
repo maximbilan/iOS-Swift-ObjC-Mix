efaf75225e6094daaf997739bd23e6d38deec180
Maxim Bilan <maximb.mail@gmail.com>
2015-01-17 12:04:06 +0200

    Added objc project template

---
+++ objc_project/ios_objc_mix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios_objc_mix
+//
+//  Created by Maxim Bilan on 1/17/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
