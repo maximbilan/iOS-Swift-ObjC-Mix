8f65474d308320ecb281d83969b42d692df3adb0
Maxim Bilan <maximb.mail@gmail.com>
2015-01-18 10:19:38 +0200

    Code review

--- swift_project/ios_swift_mix/ObjCViewController.m
+++ swift_project/ios_swift_mix/ObjCViewController.m
@@ -14,13 +14,16 @@
 
 @implementation ObjCViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
     NSLog(@"Obj C Controller");
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
